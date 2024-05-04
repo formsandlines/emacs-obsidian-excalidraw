1cb9863a74a26d7410ab557a028fef42d24c196b
Peter Hofmann <peter.hofmann@formsandlines.eu>
2024-05-04 02:17:08 +0200

    Added options for default name, timestamp prefix & relative paths
    Users can now customize the package to:
    - specify a default name for the file in case none is provided
    - automatically prepend a timestamp to the filename
    - use a relative path instead of an absolute one for the link insertion

--- emacs-obsidian-excalidraw.el
+++ emacs-obsidian-excalidraw.el
@@ -53,6 +53,21 @@
   :group 'emacs-obsidian-excalidraw
   )
 
+(defcustom emacs-obsidian-excalidraw-default-name "drawing"
+  "filename to use when input field is left empty, default is “drawing”"
+  :type 'string
+  :group 'emacs-obsidian-excalidraw)
+
+(defcustom emacs-obsidian-excalidraw-timestamp nil
+  "prepend timestamp to filenames, default nil"
+  :type 'boolean
+  :group 'emacs-obsidian-excalidraw)
+
+(defcustom emacs-obsidian-excalidraw-relative-paths nil
+  "use relative instead of absolute paths to link image files, default  nil"
+  :type 'boolean
+  :group 'emacs-obsidian-excalidraw)
+
 (defun emacs-obsidian-excalidraw-get-link-at-point-raw ()
   (if (derived-mode-p 'markdown-mode)
 		   (markdown-link-url)
@@ -78,22 +93,31 @@
 (defun emacs-obsidian-excalidraw-create (name)
   "create excalidraw file through obsidian url scheme"
   (interactive "MInput Draw Name:")
-  (let ((exfile (expand-file-name
-		 (format "%s.excalidraw.md" name)
-		 emacs-obsidian-excalidraw-vault-dir))
-	(imgfile (expand-file-name
-		  (format "%s.excalidraw.%s" name emacs-obsidian-excalidraw-image-format)
-		  emacs-obsidian-excalidraw-vault-dir))
-	(oblink (format "obsidian://open?vault=%s&file=%s.excalidraw.md" emacs-obsidian-excalidraw-vault name))
-	)
-    (copy-file emacs-obsidian-excalidraw-template-file exfile)
-    (cond ((derived-mode-p 'org-mode)
-	   (insert (format "[[file:%s]]" imgfile)))
-	  ((derived-mode-p 'markdown-mode)
-	   (insert (format "![](%s)" imgfile)))
-	  (t
-	   (insert imgfile)))
-	  (browse-url oblink)))
+  (let* ((name (if (string-empty-p name)
+		   emacs-obsidian-excalidraw-default-name
+		 name))
+	 (name (if emacs-obsidian-excalidraw-timestamp
+		   (format "%s-%s" (format-time-string "%Y%m%d%H%M%S") name)
+		 name)))
+    (let ((exfile (expand-file-name
+		   (format "%s.excalidraw.md" name)
+		   emacs-obsidian-excalidraw-vault-dir))
+	  (imgfile (expand-file-name
+		    (format "%s.excalidraw.%s" name emacs-obsidian-excalidraw-image-format)
+		    emacs-obsidian-excalidraw-vault-dir))
+	  (oblink (format "obsidian://open?vault=%s&file=%s.excalidraw.md" emacs-obsidian-excalidraw-vault name))
+	  )
+      (copy-file emacs-obsidian-excalidraw-template-file exfile)
+      (let ((imgfile (if emacs-obsidian-excalidraw-relative-paths
+			 (file-relative-name imgfile)
+		       imgfile)))
+	(cond ((derived-mode-p 'org-mode)
+	       (insert (format "[[file:%s]]" imgfile)))
+	      ((derived-mode-p 'markdown-mode)
+	       (insert (format "![](%s)" imgfile)))
+	      (t
+	       (insert imgfile))))
+      (browse-url oblink))))
 
 ;;; autoload
 (defun emacs-obsidian-excalidraw-open-at-point ()
